111c320d2d99df26b262c6f58d3a2cd17482bbc4
Danilo Martins <dansm86@outlook.com>
2023-03-14 17:08:35 -0300

    feat/req04-implemented scrape news fn

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -4,7 +4,7 @@ import time
 
 
 # Requisito 1 initial commit
-def fetch(url: str):
+def fetch(url):
     HEADERS = {"user-agent": "Fake user-agent"}
     try:
         response = requests.get(url, headers=HEADERS, timeout=3)
@@ -18,28 +18,50 @@ def fetch(url: str):
 
 
 # Requisito 2
-def scrape_updates(html_content: str):
-    soup = BS(html_content, 'lxml')
-    articles = soup.find_all('article', class_='entry-preview')
+def scrape_updates(html_content):
+    soup = BS(html_content, "lxml")
+    articles = soup.find_all("article", class_="entry-preview")
     links = []
     for article in articles:
-        article_link = article.find('h2', class_='entry-title').a['href']
+        article_link = article.find("h2", class_="entry-title").a["href"]
         links.append(article_link)
     return links
 
 
 # Requisito 3
 def scrape_next_page_link(html_content):
-    soup = BS(html_content, 'lxml')
+    soup = BS(html_content, "lxml")
     try:
-        return soup.find('a', class_='next')['href']
+        return soup.find("a", class_="next")["href"]
     except TypeError:
         return None
 
 
 # Requisito 4
 def scrape_news(html_content):
-    """Seu código deve vir aqui"""
+    soup = BS(html_content, "lxml")
+    page_url = soup.head.find("link", attrs={"rel": "canonical"})["href"]
+    page_title = soup.h1.string.strip()
+    author = soup.find("span", class_="author").a.string
+    summary = soup.find("div", class_="entry-content").p.get_text().strip()
+    reading_time = (
+        soup.find("li", class_="meta-reading-time").get_text().split(" ")[0]
+    )
+    date = soup.find("li", class_="meta-date").string
+    category = (
+        soup.find("div", class_="meta-category")
+        .find("span", class_="label")
+        .string
+    )
+    return {
+        "url": page_url,
+        "title": page_title,
+        "writer": author,
+        "summary": summary,
+        "reading_time": int(reading_time),
+        "timestamp": date,
+        "category": category,
+    }
 
 
 # Requisito 5
