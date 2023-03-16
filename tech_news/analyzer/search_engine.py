19c2bb826f56c5a5b43c12dd622438295fb20fb5
Danilo Martins <dansm86@outlook.com>
2023-03-16 19:58:16 -0300

    feat/req07-implemented search_by_title fn

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -1,6 +1,15 @@
+from tech_news.database import db
+
+
 # Requisito 7
 def search_by_title(title):
-    """Seu código deve vir aqui"""
+    query = {"title": {"$regex": title, "$options": "i"}}
+    fields = {"_id": 0, "title": 1, "url": 1}
+    result = db.news.find(query, fields)
+    tupled_list = [
+        (news["title"], news["url"]) for news in list(result)
+    ]
+    return tupled_list
 
 
 # Requisito 8
